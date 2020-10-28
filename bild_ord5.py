ae6884383eb3b19881f34fdee3caf916e3f4759f
Luis Estrada <lestradad@outlook.com>
2020-10-28 18:35:25 +0100

    slutprojekt

--- bild_ord5.py
+++ bild_ord5.py
@@ -18,28 +18,30 @@ rect_text = pygame.Rect(95, 250, 140, 32)
 color_active = pygame.Color("lightskyblue3")
 color_pasive = pygame.Color("gray15")
 color = color_pasive
+buttons_font = pygame.font.Font(None, 15)
 
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 BLUE = (0, 0, 255)
+rect_y = pygame.Rect(250, 180, 80, 40)
+rect_r = pygame.Rect(250, 130, 80, 40)
+rect_b = pygame.Rect(250, 80, 80, 40)
 
 
 class Allbuttons:
-    def __init__(self, surface, x, y, width, height, colour):
+    def __init__(self, surface, rect, colour, border):
         self.surface = surface
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+        self.rect = rect
         self.colour = colour
+        self.border = border
 
     def display_buttons(self):
-        pygame.draw.rect(self.surface, self.colour, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(self.surface, self.colour, self.rect, self.border)
 
 
-button_hint = Allbuttons(screen, 250, 130, 80, 40, RED)
-button_start = Allbuttons(screen, 250, 180, 80, 40, YELLOW)
-button_language = Allbuttons(screen, 250, 80, 80, 40, BLUE)
+button_hint = Allbuttons(screen, rect_r, RED, 4)
+button_start = Allbuttons(screen, rect_y, YELLOW, 4)
+button_language = Allbuttons(screen, rect_b, BLUE, 4)
 
 
 def main():
@@ -63,11 +65,14 @@ def main():
                 else:
                     active = False
 
+            # blue button - change language
             if 250 <= pygame.mouse.get_pos()[0] <= 330 and \
                 80 <= pygame.mouse.get_pos()[
                  1] <= 120 and event.type == pygame.MOUSEBUTTONDOWN:
                 change_language *= -1
+                count_hint = 0
 
+            # yellow button - start or next
             if correct_text and 250 <= pygame.mouse.get_pos()[0] <= 330 and \
                     180 <= pygame.mouse.get_pos()[1] <= 220 and event.type == pygame.MOUSEBUTTONDOWN:
                 user_text = ""
@@ -81,8 +86,10 @@ def main():
                 if current_txt >= len(list_choice):
                     current_txt = 1
 
+            # red button - hint
             if 250 <= pygame.mouse.get_pos()[0] <= 330 and \
-                    130 <= pygame.mouse.get_pos()[1] <= 170 and event.type == pygame.MOUSEBUTTONDOWN:
+                    130 <= pygame.mouse.get_pos()[1] <= 170 and \
+                    event.type == pygame.MOUSEBUTTONDOWN and show_text:
                 count_hint += 1
                 user_text = ""
                 lista_txt_list = list(list_choice[current_txt])
@@ -116,8 +123,28 @@ def main():
 
         if change_language == 1:
             list_choice = list_esp
+            text_start_es = buttons_font.render("SIGUIENTE", True, (0,0,0))
+            text_start_es1 = buttons_font.render("(INICIAR)", True, (0, 0, 0))
+            text_hint_es = buttons_font.render("SUGERENCIA", True, (0, 0, 0))
+            text_lang_es = buttons_font.render("(ESPAÑOL)", True, (0, 0, 0))
+            text_lang_es1 = buttons_font.render("CAMBIAR", True, (0, 0, 0))
+            screen.blit(text_start_es, (rect_y.x + 15, rect_y.y + 10))
+            screen.blit(text_start_es1, (rect_y.x + 20, rect_y.y + 20))
+            screen.blit(text_hint_es, (rect_r.x + 8, rect_r.y + 15))
+            screen.blit(text_lang_es, (rect_b.x + 15, rect_b.y + 20))
+            screen.blit(text_lang_es1, (rect_b.x + 18, rect_b.y + 10))
         else:
             list_choice = list_svensk
+            text_start_sv = buttons_font.render("NÄSTA", True, (0, 0, 0))
+            text_start_sv1 = buttons_font.render("(BÖRJA)", True, (0, 0, 0))
+            text_hint_sv = buttons_font.render("LEDTRÅD", True, (0, 0, 0))
+            text_lang_sv = buttons_font.render("(SVENSKA)", True, (0, 0, 0))
+            text_lang_sv1 = buttons_font.render("BYTA", True, (0, 0, 0))
+            screen.blit(text_start_sv, (rect_y.x + 23, rect_y.y + 10))
+            screen.blit(text_start_sv1, (rect_y.x + 20, rect_y.y + 20))
+            screen.blit(text_hint_sv, (rect_r.x + 18, rect_r.y + 15))
+            screen.blit(text_lang_sv, (rect_b.x + 15, rect_b.y + 20))
+            screen.blit(text_lang_sv1, (rect_b.x + 27, rect_b.y + 10))
 
         if not correct_text:
             if user_text.lower() == list_choice[current_txt] and list_choice[current_txt] is not None:
